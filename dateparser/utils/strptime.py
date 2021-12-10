00e40e9527987e8ccbc28ef9f48eba8d7c4f133a
Benoît HERVIER <b@rvier.fr>
2021-12-10 22:51:15 +0100

    revert to imp

--- dateparser/utils/strptime.py
+++ dateparser/utils/strptime.py
@@ -34,12 +34,11 @@ def patch_strptime():
         _strptime_spec.loader.exec_module(_calendar)
         sys.modules["calendar_patched"] = _calendar
     else:
-        _strptime = _strptime_spec.loader.load_module()
-        sys.modules["strptime_patched"] = _strptime
-        _calendar = _strptime_spec.loader.load_module()
+        _strptime = imp.load_module("strptime_patched", *imp.find_module("_strptime"))
+        _calendar = imp.load_module("calendar_patched", *imp.find_module("_strptime"))
         sys.modules["calendar_patched"] = _calendar
-        # _strptime = imp.load_module("strptime_patched", *imp.find_module("_strptime"))
-        # _calendar = imp.load_module("calendar_patched", *imp.find_module("_strptime"))
+        sys.modules["strptime_patched"] = _strptime
+
     _strptime._getlang = lambda: ("en_US", "UTF-8")
     _strptime.calendar = _calendar
     _strptime.calendar.day_abbr = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"]
